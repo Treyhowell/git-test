0ed3968303f1ec0b4306f9146ff6f2b6dfbf0248
Treyhowell <terjhowe@ut.utm.edu>
2018-11-27 17:09:17 -0600

    main commit: adding project new main file .

--- git_test/main.cpp
+++ git_test/main.cpp
@@ -1,7 +1,27 @@
+//
+//  main.cpp
+//
+//  Created by Trey howell on 11/27/18.
+//
+
 #include <iostream>
 #include <iomanip>
+int sum(int num);
 using namespace std;
-int main(){
-  cout << "Hello World" << endl;
+int main() {
+  int num;
+  cout << "enter a number ";
+  cin >> num;
+  cout << endl;
+  cout << "here is the sum of your numer starting from 1: ";
+  cout << sum(num);
+  cout << endl;
+
   return 0;
 }
+int sum(int num){
+  if(num != 0)
+    return num + sum(num- 1);
+  return 0;
+}
+
